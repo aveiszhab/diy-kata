33bb0456de0ced04fc2d4826f064e8eccc5332e1
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-03-09 20:42:48 +0000

    FizzBuzz Complete

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -5,9 +5,18 @@ describe("fizzBuzz", () => {
     expect(fizzBuzz(3)).toBe('Fizz');
   });
 
-  xit("returns Buzz when passed a multiple of 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
 
-  xit("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(30)).toBe('FizzBuzz');
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
+  });
 
-  xit("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(2)).toBe(2);
+    expect(fizzBuzz(11)).toBe(11);
+  });
 });
