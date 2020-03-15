f94db39e2bc1555ea5ff0865f74a3f887ae08cc9
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-03-15 14:54:19 +0000

    ReachDest Completed

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,5 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  return `I should be there in ${Math.ceil((distance / speed) * 2) / 2} hours.`;
+};
 
 module.exports = reachDestination;
