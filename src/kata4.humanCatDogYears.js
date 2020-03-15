26c411afc7510b168fd2f7ac0a849daf314f59d8
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-03-15 14:25:05 +0000

    HCDY completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,23 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = humanYears => {
+  return [
+    humanYears,
+    (humanYears - 1 ? 16 : 11) + 4 * humanYears,
+    (humanYears - 1 ? 14 : 10) + 5 * humanYears
+  ];
+
+  /* let dogYears;
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  } else {
+    catYears = 15 + 9 + (humanYears - 2) * 4;
+    dogYears = 15 + 9 + (humanYears - 2) * 5;
+  }
+
+  return [number, catYears, dogYears]; */
+};
 
 module.exports = humanCatDogYears;
